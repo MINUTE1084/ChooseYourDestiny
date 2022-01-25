b9e72ce154c80e31ef033d7ed64eca814cd2e9a1
MINUTE1084 <swpro1004@gmail.com>
2022-01-25 12:58:42 +0900

    Difficult: Impossible

--- main.lua
+++ main.lua
@@ -4,9 +4,10 @@ local cahllengeEasy = Isaac.GetChallengeIdByName("Choose your destiny (Easy)")
 local cahllengeNormal = Isaac.GetChallengeIdByName("Choose your destiny (Normal)")
 local cahllengeHard = Isaac.GetChallengeIdByName("Choose your destiny (Hard)")
 local cahllengeInsane = Isaac.GetChallengeIdByName("Choose your destiny (Insane)")
+local cahllengeImpossible = Isaac.GetChallengeIdByName("Choose your destiny (Impossible)")
 
 function mod:InitalizeChallenge(isContinue)
-	if((game.Challenge == cahllengeEasy or game.Challenge == cahllengeNormal or game.Challenge == cahllengeHard or game.Challenge == cahllengeInsane) and 
+	if((game.Challenge == cahllengeEasy or game.Challenge == cahllengeNormal or game.Challenge == cahllengeHard or game.Challenge == cahllengeInsane or game.Challenge == cahllengeImpossible) and 
 	isContinue == false) then 
 		gameState = 0 
 	end
@@ -17,12 +18,11 @@ function mod:UseGenesis()
 	local currentRoom = game:GetLevel():GetCurrentRoomIndex()
 	local startingRoom = game:GetLevel():GetStartingRoomIndex()
 
-	if currentRoom == startingRoom then return 0 end
-	if gameState == 0 then
+	if currentRoom ~= startingRoom and gameState == 0 then
 		gameState = 1
 		
 		local itemCount = 0
-		if(game.Challenge == cahllengeEasy) then itemCount = 40 
+		if(game.Challenge == cahllengeEasy or game.Challenge == cahllengeImpossible) then itemCount = 40 
 		elseif(game.Challenge == cahllengeNormal) then itemCount = 30 
 		elseif(game.Challenge == cahllengeHard) then itemCount = 25 
 		elseif(game.Challenge == cahllengeInsane) then itemCount = 15
@@ -37,14 +37,9 @@ mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.UseGenesis)
 
 function mod:RemoveCollectibles(pickup)
 	if (pickup.Variant == 100 and not (pickup.SubType == 626 or pickup.SubType == 627 or pickup.SubType == 688) and
-	(game.Challenge == cahllengeEasy or game.Challenge == cahllengeNormal or game.Challenge == cahllengeHard or game.Challenge == cahllengeInsane)) then 
-		if (game:GetLevel():GetStage() ~= LevelStage.STAGE1_1) then 
+	(game.Challenge == cahllengeEasy or game.Challenge == cahllengeNormal or game.Challenge == cahllengeHard or game.Challenge == cahllengeInsane or game.Challenge == cahllengeImpossible)) then 
+		if (game:GetLevel():GetStage() ~= LevelStage.STAGE1_1 or game:GetRoom():GetType() ~= RoomType.ROOM_ISAACS) then 
 			pickup:Remove() 
-
-		elseif (gameState == 1) then
-			gameState = 2
-			local player = Isaac.GetPlayer(0)
-			player:AddCollectible(CollectibleType.COLLECTIBLE_CHAOS)
 		end
 	end
 end
